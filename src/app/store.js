27b2ba98b1bf27c23f02b04c45f0e982f96c1c89
Ciro Perez <ciro@bmiahorro.com>
2022-06-10 23:08:22 +0200

    Generanda estructura inicial para usar REDUX

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import searchReducer from "../features/searchSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    search: searchReducer,
   },
 });
---
+++ src/features/searchSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  searchString: "",
+};
+
+export const searchSlice = createSlice({
+  name: "search",
+  initialState,
+  reducers: {
+    setSearchString: (state, action) => {
+      state.searchString = action.payload;
+    },
+  },
+});
+
+export const { setSearchString } = searchSlice.actions;
+
+export const selectSearch = (state) => state.search.searchString;
+
+export default searchSlice.reducer;
